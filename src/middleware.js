f923b937879b60609182e343f2c544902e71b987
Md Nahid Hasan <106926548+nahidthedeveloper@users.noreply.github.com>
2024-12-01 05:20:38 -0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,26 +1,26 @@
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-export function middleware(request) {
-    const authenticate = request.cookies.get('next-auth.session-token')?.value;
-    const { nextUrl, url } = request;
+// export function middleware(request) {
+//     const authenticate = request.cookies.get('next-auth.session-token')?.value;
+//     const { nextUrl, url } = request;
 
-    const loginUserCanNotAccess = ['/auth/login', '/auth/signup'];
-    const unAuthorizeUserCanNotAccess = ['/'];
+//     const loginUserCanNotAccess = ['/auth/login', '/auth/signup'];
+//     const unAuthorizeUserCanNotAccess = ['/'];
 
-    if (authenticate) {
-        if (loginUserCanNotAccess.includes(nextUrl.pathname)) {
-            return NextResponse.redirect(new URL('/', url));
-        }
-    } else {
-        if (unAuthorizeUserCanNotAccess.includes(nextUrl.pathname)) {
-            return NextResponse.redirect(new URL('/auth/login', url));
-        }
-    }
-}
+//     if (authenticate) {
+//         if (loginUserCanNotAccess.includes(nextUrl.pathname)) {
+//             return NextResponse.redirect(new URL('/', url));
+//         }
+//     } else {
+//         if (unAuthorizeUserCanNotAccess.includes(nextUrl.pathname)) {
+//             return NextResponse.redirect(new URL('/auth/login', url));
+//         }
+//     }
+// }
 
-export const config = {
-    matcher: [
-        '/auth/:path*',
-        '/',
-    ],
-};
+// export const config = {
+//     matcher: [
+//         '/auth/:path*',
+//         '/',
+//     ],
+// };

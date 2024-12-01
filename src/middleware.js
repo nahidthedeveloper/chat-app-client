1297d2293624187e8c06406bd3ce3796c77e0442
Md Nahid Hasan <106926548+nahidthedeveloper@users.noreply.github.com>
2024-12-01 05:59:26 -0800

    fix middleware issue

--- src/middleware.js
+++ src/middleware.js
@@ -1,26 +1,23 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
     const authenticate = request.cookies.get('next-auth.session-token')?.value;
-    const { nextUrl, url } = request;
+    const { nextUrl } = request;
 
     const loginUserCanNotAccess = ['/auth/login', '/auth/signup'];
     const unAuthorizeUserCanNotAccess = ['/'];
 
     if (authenticate) {
         if (loginUserCanNotAccess.includes(nextUrl.pathname)) {
-            return NextResponse.redirect(new URL('/', url));
+            return NextResponse.redirect(new URL('/', request.url));
         }
     } else {
         if (unAuthorizeUserCanNotAccess.includes(nextUrl.pathname)) {
-            return NextResponse.redirect(new URL('/auth/login', url));
+            return NextResponse.redirect(new URL('/auth/login', request.url));
         }
     }
 }
 
 export const config = {
-    matcher: [
-        '/auth/:path*',
-        '/',
-    ],
+    matcher: ['/auth/:path*', '/'],
 };

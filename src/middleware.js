295a03c97dfd76ba07fc862eec755badf5403dc4
Md Nahid Hasan <106926548+nahidthedeveloper@users.noreply.github.com>
2024-12-01 04:54:25 -0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,26 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-    const authenticate = request.cookies.get('next-auth.session-token')?.value
-    const { nextUrl, url } = request
+    const authenticate = request.cookies.get('next-auth.session-token')?.value;
+    const { nextUrl, url } = request;
 
-    const loginUserCanNotAccess = ['/auth/login', '/auth/signup']
-    const UnAuthorizeUserCanNotAccess = ['/']
+    const loginUserCanNotAccess = ['/auth/login', '/auth/signup'];
+    const unAuthorizeUserCanNotAccess = ['/'];
 
     if (authenticate) {
         if (loginUserCanNotAccess.includes(nextUrl.pathname)) {
-            return NextResponse.redirect(new URL('/', url))
+            return NextResponse.redirect(new URL('/', url));
         }
     } else {
-        if (UnAuthorizeUserCanNotAccess.includes(nextUrl.pathname)) {
-            return NextResponse.redirect(new URL('/auth/login', url))
+        if (unAuthorizeUserCanNotAccess.includes(nextUrl.pathname)) {
+            return NextResponse.redirect(new URL('/auth/login', url));
         }
     }
 }
+
+export const config = {
+    matcher: [
+        '/auth/:path*',
+        '/',
+    ],
+};
